c43b090f78be2efa7005cc38d384fc1f0ad0ae08
doraemon49 <min1593@nate.com>
2023-02-04 04:08:58 +0900

    [#4 - ]feat: "Add get 수정 전 함수, post 포트폴리오 수정"
    수정 페이지를 불러오고, 포트폴리오를 수정하는 기능 개발
    
    Resolves: #4

--- src/api/myPage.js
+++ src/api/myPage.js
@@ -94,6 +94,21 @@ app.post("/write", (req, res) => {
 });
 
 //포트폴리오 수정
-app.put("/write/:id", (req, res) => {});
-
+//app.put("/write/:id", (req, res) => {});
+app.post("update/:id", (req, res) => {
+  const sql = "UPDATE posts SET ? WHERE id = " + req.params.id;
+  con.query(sql, req.body, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    res.redirect("/"); //나의 페이지로 이동
+  });
+});
+//수정 전 데이터 불러오기
+app.get("edit/:id", (req, res) => {
+  const sql = "SELECT * FROM posts WHERE id = ?";
+  con.query(sql, [req.params.id], function (err, result, fields) {
+    if (err) throw err;
+    res.render("edit", { posts: result });
+  });
+});
 export default app;
