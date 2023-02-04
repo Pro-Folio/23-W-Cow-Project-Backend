de5bbf4db7347e51384a696e7c190ac5cbd287fd
doraemon49 <min1593@nate.com>
2023-02-04 14:55:32 +0900

    [#1 - ]feat: "Add get 포트폴리오 조회 함수"
    포트폴리오를 조회하는 기능 개발. mysql 생성 connect 방식에서, Sequelize를 활용한 방식으로 변경.
    
    Resolves #1

--- src/api/myPage.js
+++ src/api/myPage.js
@@ -2,22 +2,19 @@ import { Router } from "express";
 import { Posts } from "../../models";
 const app = Router();
 
-//포트폴리오 수정
-//app.put("/write/:id", (req, res) => {});
-app.post("update/:id", (req, res) => {
-  const sql = "UPDATE posts SET ? WHERE id = " + req.params.id;
-  con.query(sql, req.body, function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-    res.redirect("/"); //나의 페이지로 이동
-  });
+//마의페이지 (나의 모든 포트폴리오) 조회 read
+app.get("/", async (req, res) => {
+  const posts = await Posts.findAll({}).catch((err) => console.log(err));
+  res.status(200).send(posts);
 });
-//수정 전 데이터 불러오기
-app.get("edit/:id", (req, res) => {
-  const sql = "SELECT * FROM posts WHERE id = ?";
-  con.query(sql, [req.params.id], function (err, result, fields) {
-    if (err) throw err;
-    res.render("edit", { posts: result });
-  });
+
+//상세 포트폴리오 조회
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const post = await Posts.findOne({ where: { id: id } }).catch((err) =>
+    console.log(err)
+  );
+  res.status(200).send(post);
 });
+
 export default app;
