39ec962f7683aef680dd5b6af59852f15ce172f6
doraemon49 <min1593@nate.com>
2023-02-04 15:29:54 +0900

    feat: "Add put 포트폴리오 수정 함수"
    포트폴리오를 수정하는 기능 개발. mysql 생성 connect 방식에서, Sequelize를 활용한 방식으로 변경.

--- src/api/myPage.js
+++ src/api/myPage.js
@@ -2,37 +2,13 @@ import { Router } from "express";
 import { Posts } from "../../models";
 const app = Router();
 
-//포트폴리오 작성 페이지로 이동
-app.get("/write", (req, res) => {
-  //프론트 작성 페이지
-  res.send();
-});
-
-//포트폴리오 내용 입력 받기 (작성) create
-app.post("/write", async (req, res) => {
-  const userId = req.body.userId;
-  const image = req.body.image;
-  const title = req.body.title;
-  const summary = req.body.summary;
-  const techStack = req.body.techStack;
-  const period = req.body.period;
-  const date = req.body.date;
-  const detail = req.body.detail;
-
-  const post_list = await Posts.create({
-    userId: userId,
-    title: title,
-    image: image,
-    summary: summary,
-    techStack: techStack,
-    period: period,
-    date: date,
-    detail: detail,
-  });
-
-  return res.json({
-    msg: "create 성공",
-  });
+//포트폴리오 수정 update
+app.put("/edit/:id", async (req, res) => {
+  const id = req.params.id;
+  const post = await Posts.update(req.body, { where: { id: id } }).catch(
+    (err) => console.log(err)
+  );
+  res.status(200).send(post);
 });
 
 export default app;
