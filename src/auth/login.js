79b575a5e9d4c9af79fa6310b3efb2868354d324
LeeJaeHyeok97 <hazardous10@naver.com>
2023-02-05 11:32:40 +0900

    fix: 회원가입, 로그인 응답 데이터 변경, 로그인 시 에러 처리 안되던 것 수정, 닉네임 중복검사 path variable 형식으로 변경

--- src/auth/login.js
+++ src/auth/login.js
@@ -22,7 +22,6 @@ app.post('/', async (req, res) => {
         if(password === loginCheck[0].password) {
             same = 1
         } 
-        console.log(same)
         if(same) {
             const token = sign({
                 id: loginCheck[0].id,
@@ -35,30 +34,28 @@ app.post('/', async (req, res) => {
             }
         );
             return res.json({
-                "result": "success",
                 "code": 200,
                 "msg": "로그인 되었습니다.(토큰 1시간 지속)",
-                "token": token,
                 "data": {
                     "email": email,
-                    "password": null,
+                    "id": loginCheck[0].id,
+                    "token": token
                 }
             });
         }
     
-    else if (same === false) {
-        return res.json({
-            "result": "fail",
-            "code": 401,
-            "msg": "이메일 또는 비밀번호 오류가 틀렸습니다."
-        })
-    };
+        else {
+            return res.json({
+                "code": 401,
+                "msg": "이메일 또는 비밀번호 오류가 틀렸습니다."
+                })
+            };
+
     };
 
 
     //등록되지 않은 이메일일 경우
     return res.json({
-        "result": "fail",
         "code": 409,
         "msg": "등록되지 않은 이메일입니다."
     })
