83ac4810b5564436cee47026320716ffb854ab82
doraemon49 <min1593@nate.com>
2023-02-10 22:41:11 +0900

    feat: 검색 기능 - teckStack
    querystring을 통해 받은 teckStack을 검색합니다.

--- src/api/mainPage.js
+++ src/api/mainPage.js
@@ -1,7 +1,27 @@
-import { Router } from "express";
+import { query, Router } from "express";
 import { Posts } from "../../models";
+//techStack 모델 추가
+const sequelize = require("sequelize");
+const Op = sequelize.Op;
 
 const app = Router();
 
+app.get("/", async (req, res, next) => {
+  const searchWord = req.query.techStack; //querystring으로 techStack 받아오기
+  console.log("검색 단어는", searchWord);
 
+  const searchQuery = {
+    where: {
+      techStack: {
+        [Op.like]: "%" + searchWord + "%",
+      },
+    },
+  };
+
+  const techStackSearch = await Posts.findAll(searchQuery)
+    .then((result) => res.json(result))
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 export default app;
