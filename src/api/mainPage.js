349fa05cf670aabcb30815493e4e7666cc7bec45
doraemon49 <min1593@nate.com>
2023-02-11 22:13:44 +0900

    feat: techStack 검색

--- src/api/mainPage.js
+++ src/api/mainPage.js
@@ -1,5 +1,7 @@
 import { query, Router } from "express";
 import { Posts } from "../../models";
+import { User } from "../../models";
+
 //techStack 모델 추가
 const sequelize = require("sequelize");
 const Op = sequelize.Op;
@@ -7,21 +9,60 @@ const Op = sequelize.Op;
 const app = Router();
 
 app.get("/", async (req, res, next) => {
-  const searchWord = req.query.techStack; //querystring으로 techStack 받아오기
-  console.log("검색 단어는", searchWord);
+  //   const techStack = req.query.techStack; //querystring으로 techStack 받아오기
+  //   console.log("검색 테크스택은", techStack);
 
+  const queryWord = req.query.searchWord;
+  const searchWord = queryWord.replace(" ", "%"); //띄어쓰기 사용하여 검색하였을 때.
+  console.log("검색 단어는", searchWord);
   const searchQuery = {
     where: {
-      techStack: {
-        [Op.like]: "%" + searchWord + "%",
-      },
+      techStack: { [Op.like]: `%${searchWord}%` },
     },
   };
-
   const techStackSearch = await Posts.findAll(searchQuery)
     .then((result) => res.json(result))
     .catch((err) => {
       res.status(500).send(err);
     });
+
+  //   const type = req.query.type;
+  //   console.log("타입은", type);
+  //   const query = req.query.query;
+  //   console.log("검색 단어는", query);
+
+  //   if (type == "techStack") {
+  //     console.log("타입은 테크스택으로 확인됨.");
+  //     const searchQuery = {
+  //       where: {
+  //         techStack: {
+  //           [Op.like]: "%" + query + "%",
+  //         },
+  //       },
+  //     };
+  //     const techStackSearch = await Posts.findAll(searchQuery)
+  //       .then((result) => res.json(result))
+  //       .catch((err) => {
+  //         res.status(500).send(err);
+  //       });
+  //   } else if (type == "nickname") {
+  //     const searchQuery = {
+  //       include: [
+  //         {
+  //           model: User,
+  //           where: {
+  //             nickname: {
+  //               [Op.like]: "%" + query + "%",
+  //             },
+  //           },
+  //         },
+  //       ],
+  //     };
+  //     const techStackSearch = await Posts.findAll(searchQuery)
+  //       .then((result) => res.json(result))
+  //       .catch((err) => {
+  //         res.status(500).send(err);
+  //       });
+  //   }
 });
 export default app;
