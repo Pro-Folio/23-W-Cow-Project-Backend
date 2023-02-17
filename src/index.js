5fff8a2759000fbd9d42330cd2b0a66078e587e8
89882 <pgh123166@naver.com>
2023-02-18 03:14:51 +0900

    fix: CORS origin 허용 및 이미지 업로드 limit 추가

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
-import express from "express";
-import api from "./api";
-import auth from "./auth";
-import dotenv from "dotenv";
-import cors from "cors";
+import express from 'express';
+import api from './api';
+import auth from './auth';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = 8080;
 
-
-const { sequelize } = require("../models");
+const { sequelize } = require('../models');
 
 sequelize
   .sync({ force: false })
   .then(() => {
-    console.log("데이터베이스 연결 성공");
+    console.log('데이터베이스 연결 성공');
   })
   .catch((err) => {
     console.log(err);
   });
 
-app.use(cors({
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: 'htp://localhost:3000', // 출처 허용 옵션
+    credentials: true,
+  })
+);
 
-app.use(express.json());
-app.use("/api", api);
-app.use("/auth", auth);
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: false }));
+
+app.use('/api', api);
+app.use('/auth', auth);
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
