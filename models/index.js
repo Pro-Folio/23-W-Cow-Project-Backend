9ae802d878e3733556bcc9d958869c63fc0c8434
89882 <pgh123166@naver.com>
2023-02-18 03:11:00 +0900

    fix: techStack 테이블 연결 코드 삭제

--- models/index.js
+++ models/index.js
@@ -1,12 +1,11 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 
 //1
-const Posts = require("./posts");
-const User = require("./user");
-const TechStack = require("./techStack");
+const Posts = require('./posts');
+const User = require('./user');
 
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config")[env];
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config')[env];
 const db = {};
 
 const sequelize = new Sequelize( //config의 db정보와 연결
@@ -21,16 +20,13 @@ db.sequelize = sequelize;
 //2
 db.Posts = Posts;
 db.User = User;
-db.TechStack = TechStack;
 
 //3
 Posts.init(sequelize);
 User.init(sequelize);
-TechStack.init(sequelize);
 
 //4
 Posts.associate(db);
 User.associate(db);
-TechStack.associate(db);
 
 module.exports = db;
