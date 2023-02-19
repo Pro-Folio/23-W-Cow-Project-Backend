99c8828fdbdf795ca07fef244fab2118d1654c6c
doraemon49 <min1593@nate.com>
2023-02-19 16:11:43 +0900

    feat: 검색 기능 구현

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import Portfolio from "./portfolio";
 import My from "./my";
-
-
+import Search from "./search";
 
 const app = Router();
 
 app.use("/portfolio", Portfolio);
 app.use("/my", My);
+app.use("/search", Search);
 
 export default app;
